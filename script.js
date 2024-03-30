35e5fc835e55b9df110b39a55d1dd24dcbd60bbf
ThalyaC1 <nathalie.mamain@yahoo.fr>
2024-03-30 01:49:35 +0100

    Maj des 3 catégories (avec progression)

--- script.js
+++ script.js
@@ -102,10 +102,10 @@ async function fetchClassementMystery(){
 
   for(let i=0; i<nombreFilms; i++){
     if(nombreFilms <= dataMysteryNbre){
-      const dataMystery = dataMysteryTotal.results[i];
+      const dataFilmMystery = dataMysteryTotal.results[i];
       //console.log(dataMystery)
       //let dataAdressFilmMystery = dataMystery.url;
-      dataListMystery.push(dataMystery);
+      dataListMystery.push(dataFilmMystery); // changer le nom de datamystery pas assez clair
       console.log(dataListMystery.length);
       mystery(dataListMystery);
     }
@@ -113,10 +113,10 @@ async function fetchClassementMystery(){
     else{
       
       for(let i=0; i<dataMysteryNbre; i++){
-        const dataMystery = dataMysteryTotal.results[i];
+        const dataFilmMystery = dataMysteryTotal.results[i];
         //console.log(dataMystery)
         //let dataAdressFilmMystery = dataMystery.url;
-        dataListMystery.push(dataMystery);
+        dataListMystery.push(dataFilmMystery);
       }
       //for(let i=dataMysteryNbre; i<nombreFilms; i++){
       j=nombreFilms-dataListMystery.length;
@@ -144,63 +144,179 @@ async function fetchClassementMystery(){
 function mystery(dataListMystery){
   const filmUnMystery = dataListMystery[0];
   let elementTitreFilmUnMystery = filmUnMystery.title;
-  let c1F1Titre = document.querySelector("#cat1-1 .bandeau h2");
+  let c1F1Titre = document.querySelector("#cat1-0 .bandeau h2");
   c1F1Titre.textContent = elementTitreFilmUnMystery;
   let elementImagFilmUnMystery = filmUnMystery.image_url;
-  let c1F1Image = document.querySelector("#cat1-1 img");
+  let c1F1Image = document.querySelector("#cat1-0 img");
   c1F1Image.src = elementImagFilmUnMystery;
   c1F1Image.alt = c1F1Titre.textContent;
 
   const filmDeuxMystery = dataListMystery[1];
   let elementTitreFilmDeuxMystery = filmDeuxMystery.title;
-  let c1F2Titre = document.querySelector("#cat1-2 .bandeau h2");
+  let c1F2Titre = document.querySelector("#cat1-1 .bandeau h2");
   c1F2Titre.textContent = elementTitreFilmDeuxMystery;
   let elementImagFilmDeuxMystery = filmDeuxMystery.image_url;
-  let c1F2Image = document.querySelector("#cat1-2 img");
+  let c1F2Image = document.querySelector("#cat1-1 img");
   c1F2Image.src = elementImagFilmDeuxMystery;
   c1F2Image.alt = c1F2Titre.textContent;
 
   const filmTroisMystery = dataListMystery[2];
   let elementTitreFilmTroisMystery = filmTroisMystery.title;
-  let c1F3Titre = document.querySelector("#cat1-3 .bandeau h2");
+  let c1F3Titre = document.querySelector("#cat1-2 .bandeau h2");
   c1F3Titre.textContent = elementTitreFilmTroisMystery;
   let elementImagFilmTroisMystery = filmTroisMystery.image_url;
-  let c1F3Image = document.querySelector("#cat1-3 img");
+  let c1F3Image = document.querySelector("#cat1-2 img");
   c1F3Image.src = elementImagFilmTroisMystery;
   c1F3Image.alt = c1F3Titre.textContent;
 
   const filmQuatreMystery = dataListMystery[3];
   let elementTitreFilmQuatreMystery = filmQuatreMystery.title;
-  let c1F4Titre = document.querySelector("#cat1-4 .bandeau h2");
+  let c1F4Titre = document.querySelector("#cat1-3 .bandeau h2");
   c1F4Titre.textContent = elementTitreFilmQuatreMystery;
   let elementImagFilmQuatreMystery = filmQuatreMystery.image_url;
-  let c1F4Image = document.querySelector("#cat1-4 img");
+  let c1F4Image = document.querySelector("#cat1-3 img");
   c1F4Image.src = elementImagFilmQuatreMystery;
   c1F4Image.alt = c1F4Titre.textContent;
 
   const filmCinqMystery = dataListMystery[4];
   let elementTitreFilmCinqMystery = filmCinqMystery.title;
-  let c1F5Titre = document.querySelector("#cat1-5 .bandeau h2");
+  let c1F5Titre = document.querySelector("#cat1-4 .bandeau h2");
   c1F5Titre.textContent = elementTitreFilmCinqMystery;
   let elementImagFilmCinqMystery = filmCinqMystery.image_url;
-  let c1F5Image = document.querySelector("#cat1-5 img");
+  let c1F5Image = document.querySelector("#cat1-4 img");
   c1F5Image.src = elementImagFilmCinqMystery;
   c1F5Image.alt = c1F5Titre.textContent;
 
   const filmSixMystery = dataListMystery[5];
   let elementTitreFilmSixMystery = filmSixMystery.title;
-  let c1F6Titre = document.querySelector("#cat1-6 .bandeau h2");
+  let c1F6Titre = document.querySelector("#cat1-5 .bandeau h2");
   c1F6Titre.textContent = elementTitreFilmSixMystery;
   let elementImagFilmSixMystery = filmSixMystery.image_url;
-  let c1F6Image = document.querySelector("#cat1-6 img");
+  let c1F6Image = document.querySelector("#cat1-5 img");
   c1F6Image.src = elementImagFilmSixMystery;
   c1F6Image.alt = c1F6Titre.textContent;
+}
+
+async function fetchClassementFantasy(){
+  const apiEndpointFantasy = "http://localhost:8000/api/v1/titles/?genre=Fantasy&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
+  const responseFantasy = await fetch(apiEndpointFantasy);
+  const dataFantasyTotal = await responseFantasy.json();
+  const dataFantasyNbre = (dataFantasyTotal.results).length;
+  console.log(dataFantasyNbre);
+  let dataListFantasy = [];
+  let nombreFilms = 6;
+
+  for(let i=0; i<nombreFilms; i++){
+    if(nombreFilms <= dataFantasyNbre){
+      const dataFilmFantasy = dataFantasyTotal.results[i];
+      dataListFantasy.push(dataFilmFantasy);
+      console.log(dataListFantasy.length);
+      displayFantasyFilm(nombreFilms, dataListFantasy);
+    }
+    
+    else{
+      for(let i=0; i<dataFantasyNbre; i++){
+        const dataFilmFantasy = dataFantasyTotal.results[i];
+        dataListFantasy.push(dataFilmFantasy);
+      }
+      j=nombreFilms-dataListFantasy.length;
+      const apiEndpointFantasySuit = dataFantasyTotal.next;
+      const responseFantasySuit = await fetch(apiEndpointFantasySuit);
+      const dataFantasyTotalSuit = await responseFantasySuit.json();
+      
+      for(let k=0; k<j; k++){
+        const dataFilmFantasySuit = dataFantasyTotalSuit.results[k];
+        dataListFantasy.push(dataFilmFantasySuit);
+      }
+      displayFantasyFilm(nombreFilms, dataListFantasy);
+      console.log(displayFantasyFilm);
+    }
+  }
+}
+
+function displayFantasyFilm(nombreFilms, dataListFantasy){
+  let i=0;
+  while (i < nombreFilms) {
+    console.log(i);
+    let emplacementDomTitre = "#cat2-"+i+" .bandeau h2";
+    console.log(emplacementDomTitre);
+    let emplacementDomImag = "#cat2-"+i+ " img";
+    let filmFantasy = dataListFantasy[i];
+    let elementTitreFilmFantasy = filmFantasy.title;
+    console.log(elementTitreFilmFantasy);
+    let titreFilm = document.querySelector(emplacementDomTitre);
+    console.log(titreFilm);
+    titreFilm.textContent = elementTitreFilmFantasy;
+    let elementImagFilmFantasy = filmFantasy.image_url;
+    let imagFilm = document.querySelector(emplacementDomImag);
+    imagFilm.src = elementImagFilmFantasy;
+    imagFilm.alt = titreFilm.textContent;
+    i++
+  }
+}
+
+async function fetchClassementSciFi(){
+  const apiEndpointSci = "http://localhost:8000/api/v1/titles/?genre=Sci-Fi&sort_by=-imdb_score&director=&director_contains=&writer=&writer_contains=&actor=&actor_contains=&country=&country_contains=&lang=&lang_contains=&company=&company_contains=&rating=&rating_contains=";
+  const responseSci = await fetch(apiEndpointSci);
+  const dataSciTotal = await responseSci.json();
+  const dataSciNbre = (dataSciTotal.results).length;
+  console.log(dataSciNbre);
+  let dataListSci = [];
+  let nombreFilms = 6;
+
+  for(let i=0; i<nombreFilms; i++){
+    if(nombreFilms <= dataSciNbre){
+      const dataFilmSci = dataSciTotal.results[i];
+      dataListSci.push(dataFilmSci);
+      console.log(dataListSci.length);
+      displayFilm(nombreFilms, dataListSci);
+    }
+    
+    else{
+      for(let i=0; i<dataSciNbre; i++){
+        const dataFilmSci = dataSciTotal.results[i];
+        dataListSci.push(dataFilmSci);
+      }
+      j=nombreFilms-dataListSci.length;
+      const apiEndpointSciSuit = dataSciTotal.next;
+      const responseSciSuit = await fetch(apiEndpointSciSuit);
+      const dataSciTotalSuit = await responseSciSuit.json();
+      
+      for(let k=0; k<j; k++){
+        const dataFilmSciSuit = dataSciTotalSuit.results[k];
+        dataListSci.push(dataFilmSciSuit);
+      }
+      displayFilm(nombreFilms, dataListSci);
+      console.log(displayFantasyFilm);
+    }
+  }
+}
+
+function displayFilm(nombreFilms, dataList){
+  let i=0;
+  while (i < nombreFilms) {
+    console.log(i);
+    let emplacementDomTitre = "#cat3-"+i+" .bandeau h2";
+    console.log(emplacementDomTitre);
+    let emplacementDomImag = "#cat3-"+i+ " img";
+    let film = dataList[i];
+    let elementTitreFilm = film.title;
+    console.log(elementTitreFilm);
+    let titreFilm = document.querySelector(emplacementDomTitre);
+    console.log(titreFilm);
+    titreFilm.textContent = elementTitreFilm;
+    let elementImagFilm = film.image_url;
+    let imagFilm = document.querySelector(emplacementDomImag);
+    imagFilm.src = elementImagFilm;
+    imagFilm.alt = titreFilm.textContent;
+    i++
+  }
 }
 
 // _________
 
 //BEST FILM
-fetchClassementGeneral()
+fetchClassementGeneral();
 let bestFilmBouton = document.querySelector(".content button.bouton a");
 console.log(bestFilmBouton);
 
@@ -210,91 +326,77 @@ notation exemple : c1F1 = catégorie 1 Film 1 */
 
 // Catégorie 1 : Mystery
 
-fetchClassementMystery()
-let c1F1Bouton = document.querySelector("#cat1-1 button.bouton a")
-console.log(c1F1Bouton)
+fetchClassementMystery();
+let c1F1Bouton = document.querySelector("#cat1-0 button.bouton a");
+console.log(c1F1Bouton);
 
 
 
 
 //Film 2
-let c1F2Bouton = document.querySelector("#cat1-2 button.bouton a")
-console.log(c1F2Bouton)
+let c1F2Bouton = document.querySelector("#cat1-1 button.bouton a");
+console.log(c1F2Bouton);
 
 //Film 3
-let c1F3Bouton = document.querySelector("#cat1-3 button.bouton a")
-console.log(c1F3Bouton)
+let c1F3Bouton = document.querySelector("#cat1-2 button.bouton a");
+console.log(c1F3Bouton);
 
 //Film 4
-let c1F4Bouton = document.querySelector("#cat1-4 button.bouton a")
-console.log(c1F4Bouton)
+let c1F4Bouton = document.querySelector("#cat1-3 button.bouton a");
+console.log(c1F4Bouton);
 
 //Film 5
-let c1F5Bouton = document.querySelector("#cat1-5 button.bouton a")
-console.log(c1F5Bouton)
+let c1F5Bouton = document.querySelector("#cat1-4 button.bouton a");
+console.log(c1F5Bouton);
 
 //Film 6
-let c1F6Bouton = document.querySelector("#cat1-6 button.bouton a")
-console.log(c1F6Bouton)
+let c1F6Bouton = document.querySelector("#cat1-5 button.bouton a");
+console.log(c1F6Bouton);
 
 // Catégorie 2 : Fantasy
 
+fetchClassementFantasy();
+
 //Film 1
-let c2F1Titre = document.querySelector("#cat2-1 .bandeau h2")
-console.log(c2F1Titre)
 
-let c2F1Bouton = document.querySelector("#cat2-1 button.bouton a")
-console.log(c2F1Bouton)
 
-let c2F1Image = document.querySelector("#cat2-1 img")
-console.log(c2F1Image)
+let c2F1Bouton = document.querySelector("#cat2-0 button.bouton a");
+console.log(c2F1Bouton);
+
 
 //Film 2
-let c2F2Titre = document.querySelector("#cat2-2 .bandeau h2")
-console.log(c2F2Titre)
 
-let c2F2Bouton = document.querySelector("#cat2-2 button.bouton a")
-console.log(c2F2Bouton)
 
-let c2F2Image = document.querySelector("#cat2-2 img")
-console.log(c2F2Image)
+let c2F2Bouton = document.querySelector("#cat2-1 button.bouton a");
+console.log(c2F2Bouton);
+
 
 //Film 3
-let c2F3Titre = document.querySelector("#cat2-3 .bandeau h2")
-console.log(c2F3Titre)
 
-let c2F3Bouton = document.querySelector("#cat2-3 button.bouton a")
-console.log(c2F3Bouton)
 
-let c2F3Image = document.querySelector("#cat2-3 img")
-console.log(c2F3Image)
+let c2F3Bouton = document.querySelector("#cat2-2 button.bouton a");
+console.log(c2F3Bouton);
+
 
 //Film 4
-let c2F4Titre = document.querySelector("#cat2-4 .bandeau h2")
-console.log(c2F4Titre)
 
-let c2F4Bouton = document.querySelector("#cat2-4 button.bouton a")
-console.log(c1F4Bouton)
 
-let c2F4Image = document.querySelector("#cat2-4 img")
-console.log(c2F4Image)
+let c2F4Bouton = document.querySelector("#cat2-3 button.bouton a");
+console.log(c1F4Bouton);
+
 
 //Film 5
-let c2F5Titre = document.querySelector("#cat2-5 .bandeau h2")
-console.log(c2F5Titre)
 
-let c2F5Bouton = document.querySelector("#cat2-5 button.bouton a")
-console.log(c2F5Bouton)
 
-let c2F5Image = document.querySelector("#cat2-5 img")
-console.log(c2F5Image)
+let c2F5Bouton = document.querySelector("#cat2-4 button.bouton a");
+console.log(c2F5Bouton);
+
 
 //Film 6
-let c2F6Titre = document.querySelector("#cat2-6 .bandeau h2")
-console.log(c2F6Titre)
 
-let c2F6Bouton = document.querySelector("#cat2-6 button.bouton a")
-console.log(c2F6Bouton)
 
-let c2F6Image = document.querySelector("#cat2-6 img")
-console.log(c2F6Image)
+let c2F6Bouton = document.querySelector("#cat2-5 button.bouton a");
+console.log(c2F6Bouton);
+
+//category 3
+fetchClassementSciFi();
